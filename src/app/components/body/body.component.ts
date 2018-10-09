e74d51b578e0656889d8e418016b71c183fcd5e6
danieldec <danieldelrioc@gmail.com>
2018-10-08 18:03:40 -0600

    Primer app Angular

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent {
+  mostrar = true;
+  // mostrar = false;
+  frase: any = {
+    mensaje: 'Un gran poder requiere una gran responsabilidad',
+    autor: 'Ben Parker'
+  };
+  personajes: string[] = ['Spiderman', 'Venom', 'Dr. Octopus'];
+}
